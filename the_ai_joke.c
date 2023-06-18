f1f2f45c2e4b20070f47b29c688f51f210e4202e
प्रद्युम्न राज़नेकर <112924476+pradyumnarajnekar@users.noreply.github.com>
2023-06-18 23:11:04 +0530

    recreating the iconic ai joke (incomplete)

---
+++ the_ai_joke.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    char a[100], b[100], c[100], d[100];
+    printf("\nai : Make a wish");
+    printf("\npr : ");
+    scanf("%c", &a);
+    if(a[100] = "Make everything okay");
+    {
+        printf("\nai : Sorry! you've come too far.");
+        printf("\n     Make another wish");
+        printf("\npr : ");
+        scanf("%c", &b);
+        if(b[100] = "Go back");
+        {
+            printf("\nai : Calculating time to Go Back... ");
+            printf("\n     Error!! Too long. Sorry You can't Go Back now.");
+            printf("\n     Make another wish");
+            printf("\npr : ");
+            scanf("%c", &c);
+            if(c[100] = "Delete memories")
+            {
+                printf("\nai : Deleting Memories...");
+                printf("\n     Error!! File Too Big. Deletion terminated.");
+                printf("\n     The only remaining option now is to accept reality.");
+                printf("\nprx : ");
+                scanf("%c", &d);
+                if(d[100] = "Okay");
+                {
+                    printf("\nai : Accepting reality...");
+                    printf("\n     Reality Accepted.");
+                }    
+            }
+        }
+       
+    }
+return 0;
+}
